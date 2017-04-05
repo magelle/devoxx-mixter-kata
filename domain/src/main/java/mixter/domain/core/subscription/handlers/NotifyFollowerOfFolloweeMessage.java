5473e4f466c482aefc56a6360182188b24c8d12e
crevier <cedric.crevier@gmail.com>
2017-04-05 15:38:28 +0200

    Resolve test

--- domain/src/main/java/mixter/domain/core/subscription/handlers/NotifyFollowerOfFolloweeMessage.java
+++ domain/src/main/java/mixter/domain/core/subscription/handlers/NotifyFollowerOfFolloweeMessage.java
@@ -2,6 +2,7 @@ package mixter.domain.core.subscription.handlers;
 
 import mixter.domain.EventPublisher;
 import mixter.domain.core.message.events.MessageQuacked;
+import mixter.domain.core.message.events.MessageRequacked;
 import mixter.domain.core.subscription.FollowerRepository;
 import mixter.domain.core.subscription.Subscription;
 import mixter.domain.core.subscription.SubscriptionId;
@@ -31,4 +32,14 @@ public class NotifyFollowerOfFolloweeMessage {
             subscribtionId.notifyFollower(messageQuacked.getMessageId(), eventPublisher);
         });
     }
+
+    public void apply(MessageRequacked messageRequacked) {
+        UserId followee = messageRequacked.getAuthorId();
+        Set<UserId> followers = followerRepository.getFollowers(followee);
+        followers.forEach(follower -> {
+            Subscription subscribtionId = subscriptionRepository.getById(new SubscriptionId(follower, followee));
+            subscribtionId.notifyFollower(messageRequacked.getMessageId(), eventPublisher);
+        });
+    }
+
 }
