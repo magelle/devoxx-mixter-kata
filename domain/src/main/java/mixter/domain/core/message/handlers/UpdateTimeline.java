40873e9613595fc9750f68f5b4766b8f73ec1ef9
crevier <cedric.crevier@gmail.com>
2017-04-05 14:55:52 +0200

    Resolve test

--- domain/src/main/java/mixter/domain/core/message/handlers/UpdateTimeline.java
+++ domain/src/main/java/mixter/domain/core/message/handlers/UpdateTimeline.java
@@ -1,6 +1,21 @@
 package mixter.domain.core.message.handlers;
 
+import mixter.domain.core.message.TimelineMessageProjection;
+import mixter.domain.core.message.TimelineMessageRepository;
+import mixter.domain.core.message.events.MessageQuacked;
+
 public class UpdateTimeline {
-    public UpdateTimeline() {
+    private final TimelineMessageRepository repository;
+
+    public UpdateTimeline(TimelineMessageRepository timelineRepository) {
+        this.repository = timelineRepository;
+    }
+
+    public void apply(MessageQuacked messageQuacked) {
+        repository.save(new TimelineMessageProjection(
+                messageQuacked.getAuthorId(),
+                messageQuacked.getAuthorId(),
+                messageQuacked.getMessage(),
+                messageQuacked.getMessageId()));
     }
 }
