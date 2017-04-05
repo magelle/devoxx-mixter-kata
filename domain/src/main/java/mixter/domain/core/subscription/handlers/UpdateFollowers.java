c6e27dd23f38a255e1734f8b2f8e1691998a642a
crevier <cedric.crevier@gmail.com>
2017-04-05 15:23:22 +0200

    Resolve test

--- domain/src/main/java/mixter/domain/core/subscription/handlers/UpdateFollowers.java
+++ domain/src/main/java/mixter/domain/core/subscription/handlers/UpdateFollowers.java
@@ -1,10 +1,22 @@
 package mixter.domain.core.subscription.handlers;
 
 import mixter.doc.Handler;
+import mixter.domain.Event;
 import mixter.domain.core.subscription.FollowerRepository;
+import mixter.domain.core.subscription.SubscriptionId;
 import mixter.domain.core.subscription.events.UserFollowed;
 
 @Handler
 public class UpdateFollowers {
 
+    private final FollowerRepository repository;
+
+    public UpdateFollowers(FollowerRepository repository) {
+        this.repository = repository;
+    }
+
+    public void apply(UserFollowed userFollowed) {
+        SubscriptionId subscriptionId = userFollowed.getSubscriptionId();
+        repository.saveFollower(subscriptionId.getFollowee(), subscriptionId.getFollower());
+    }
 }
